8c50ded03d04396f2cb5fe620fc527bb3c131d81
sotakoira <63243610+sotakoira@users.noreply.github.com>
2021-07-14 01:01:04 +0200

    Update voterevealer.js

--- voterevealer.js
+++ voterevealer.js
@@ -25,20 +25,37 @@ function vote_cast() {
         var vote_option = Event.GetInt("vote_option");
         var name = Entity.GetName(entityid);
 		var choice = options[vote_option];
+				
 		switch (team) {
-		case 1: team = "[Spectators] "; break;
-		case 2: team = "[Terrorists] "; break;
-		case 3: team = "[Counter-Terrorists] "; break;
-		default: team = "[Vote] ";
+			case 1: team = "Spectators"; team_color = "\x01"; break;
+			case 2: team = "Terrorists"; team_color = "\x10"; break;
+			case 3: team = "Counter-Terrorists"; team_color = "\x0B"; break;
+			default: team = "Vote"; team_color = "\x01";
 		}
 		
 		if (mode == 1 || mode == 2) { //say + say_team
-		queue.push(team + name + " voted " + choice);
+		queue.push("[" + team + "] " + name + " voted " + choice);
 		return;
 		}
 
 		if (mode == 3) { //PrintChat
-		Global.PrintChat(team + name + " voted " + choice);
+		// https://github.com/perilouswithadollarsign/cstrike15_src/blob/master/game/server/cstrike15/cs_player_resource.cpp#L514-L524
+		entity_color = Entity.GetProp(entityid, "CCSPlayerResource", "m_iCompTeammateColor");
+		switch (entity_color) {
+			case 0: name_color = "\x09"; break; //yellow
+			case 1: name_color = "\x03"; break; //purple
+			case 2: name_color = "\x06"; break; //green
+			case 3: name_color = "\x0B"; break; //blue
+			case 4: name_color = "\x10"; break; //orange
+			default: name_color = "\x01";
+		}
+
+		switch (vote_option) {
+			case 0: choice_color = "\x04"; break;
+			case 1: choice_color = "\x07"; break;	
+			default: choice_color = "\x01";
+		}
+		Global.PrintChat("[" + team_color + team + "\x01] " + name_color + name + "\x01 voted " + choice_color + choice + "\x01");
 		return;
 		}
     }
